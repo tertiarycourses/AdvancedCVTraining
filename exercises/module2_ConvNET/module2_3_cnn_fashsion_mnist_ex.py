e37e46123b1dbbdce6ff065cbbb415f89ec3d73e
Alfred <angch@tertiaryinfotech.com>
2018-04-25 10:07:35 +0800

    init commit

--- exercises/module2_ConvNET/module2_3_cnn_fashsion_mnist_ex.py
+++ exercises/module2_ConvNET/module2_3_cnn_fashsion_mnist_ex.py
@@ -1,31 +1,32 @@
 # Advanced Computer Vision Training
 # Module 2 Convnet
-# MNIST CNN exercise
+# CNN Model on MNIST dataset
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 import keras
-
-
 from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D,MaxPool2D,Dropout,Flatten
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+# Step 1 Load the data
+from keras.datasets import fashion_mnist
+(X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+# (X_train, y_train), (X_test, y_test) = mnist.load_data()
 X_train = X_train.reshape(-1, 28, 28, 1)
 X_test = X_test.reshape(-1, 28, 28, 1)
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
+X_train /= 255.
+X_test /= 255.
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
-
-from keras.models import Sequential
-from keras.layers import Conv2D,MaxPool2D, Flatten, Dense
 
 # Step 2: Build the CNN Model
 
 model = Sequential()
-model.add(Conv2D(32,(3,3),input_shape=(28,28,1),
-                 activation='relu',padding = 'same'))
-
+model.add(Conv2D(32,(3,3),input_shape=(28,28,1), activation='relu',padding = 'same'))
 model.add(MaxPool2D((2,2,)))
 model.add(Conv2D(64,(3,3), activation='relu',padding = 'same'))
 model.add(MaxPool2D((2,2,)))
@@ -37,25 +38,18 @@ model.add(Flatten())
 model.add(Dense(256,activation='relu'))
 model.add(Dense(10,activation='softmax'))
 
-# print(model.summary())
+print(model.summary())
 
 # Step 3: Compile the Model
-
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 
 # Step 4: Train the Model
-
-history = model.fit(X_train,y_train,batch_size=100,
-	epochs=3,validation_data=(X_test,y_test))
-
+history = model.fit(X_train,y_train,epochs=3,
+	batch_size=100, validation_data=(X_test,y_test))
 
 # Step 5: Evalute the Model
 # loss,accuracy = model.evaluate(X_test,y_test)
 # print("Accuracy: %.2f%%" % (accuracy*100))
-
-
 
 import matplotlib.pyplot as plt
 
@@ -86,5 +80,4 @@ plt.legend()
 plt.show()
 
 # Step 6: Save the Model
-model.save('mnist_cnn_2.h5')
-
+model.save('mnist_cnn.h5')
