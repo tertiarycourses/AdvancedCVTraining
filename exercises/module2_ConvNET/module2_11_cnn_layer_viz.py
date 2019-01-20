f1af818dca688f22f9673bbf8416cb865fdb6f04
Alfred <angch@tertiaryinfotech.com>
2019-01-21 00:22:51 +0800

    viz update

--- exercises/module2_ConvNET/module2_11_cnn_layer_viz.py
+++ exercises/module2_ConvNET/module2_11_cnn_layer_viz.py
@@ -3,6 +3,10 @@
 # CNN Layer Visualization
 
 from keras.models import load_model
+import matplotlib.pyplot as plt
+from keras.preprocessing import image
+from keras.models import Model
+import numpy as np
 
 model = load_model('cats_and_dogs_small_2.h5')
 # print(model.summary())  # As a reminder.
@@ -10,26 +14,19 @@ model = load_model('cats_and_dogs_small_2.h5')
 img_path = './data/cats_and_dogs_small/test/cats/cat.1700.jpg'
 
 # We preprocess the image into a 4D tensor
-from keras.preprocessing import image
-import numpy as np
-
 img = image.load_img(img_path, target_size=(150, 150))
-img_tensor = image.img_to_array(img)
-img_tensor = np.expand_dims(img_tensor, axis=0)
-img_tensor /= 255.
-
-
-from keras import models
+img = image.img_to_array(img)
+img = np.expand_dims(img, axis=0)
+img /= 255.
 
 # Extracts the outputs of the top 8 layers:
-layer_outputs = [layer.output for layer in model.layers[:23]]
+outputs = [layer.output for layer in model.layers[:8]]
 
 # Creates a model that will return these outputs, given the model input:
-activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
-activations = activation_model.predict(img_tensor)
+activation = Model(inputs=model.input, outputs=outputs)
+activations = activation.predict(img)
 layer_activations = activations[0]
 
-import matplotlib.pyplot as plt
 for i in range(8):
     plt.figure(i)
     plt.imshow(layer_activations[0, :, :,i], cmap='viridis')
